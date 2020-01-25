26275710e035b23946f5b627fdb731ae39fe5bfc
Rafael Magno <rfl.magno02@gmail.com>
2020-01-25 18:02:38 -0300

    Scoreboard base

--- datasciencetest.py
+++ datasciencetest.py
@@ -1,15 +1,17 @@
 import pandas as pd 
 import csv
-
-uri = "https://raw.githubusercontent.com/alura-cursos/introducao-a-data-science/master/aula1.2/ratings.csv"
-df = pd.read_csv("./Scoreboard.csv")
-
-#([[5, 6], [7, 8]], columns=list('AB'))
+#Esta funçao é responsável por adicionar um novo jogador
 def newPlayer(player):
     with open('Scoreboard.csv', 'a', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
-        spamwriter.writerow(['1',player, '5'])
-newPlayer("sla")
-df.loc[df["name"]=="rafael", "wins"] = 500
-df.to_csv("Scoreboard.csv", index=False)
+        spamwriter.writerow([player, '5'])
+#Esta funçao é responsável por aumentar as vitorias de um jogador
+def addWins(name):
+    df.loc[df["name"]== name , "wins"] +=  1
+    df.to_csv("Scoreboard.csv", index=False)
+#leitura do DataFrame
+df = pd.read_csv("./Scoreboard.csv")
+df.index += 1
+#---
+
 print(df)
