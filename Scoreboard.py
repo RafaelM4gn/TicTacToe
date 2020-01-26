bc99d362107af8eb34479aa11593622be8cd7b51
Rafael Magno <rfl.magno02@gmail.com>
2020-01-25 21:39:45 -0300

    Scoreboard file fineshed

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,17 +1,23 @@
 import pandas as pd 
 import csv
 #Esta funçao é responsável por adicionar um novo jogador
+def resetDf():
+    df = pd.read_csv("./Scoreboard.csv")
+    df.index += 1
+    return df
 def newPlayer(player):
     with open('Scoreboard.csv', 'a', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
-        spamwriter.writerow([player, '5'])
+        spamwriter.writerow([player, '1'])
+    resetDf()
+    return resetDf()
 #Esta funçao é responsável por aumentar as vitorias de um jogador
 def addWins(name):
+    df = resetDf()
     df.loc[df["name"]== name , "wins"] +=  1
     df.to_csv("Scoreboard.csv", index=False)
-#leitura do DataFrame
-df = pd.read_csv("./Scoreboard.csv")
-df.index += 1
-#---
-
-print(df)
+    return resetDf()
+#Esta função é responsável por inicializar o DataFrame
+a = addWins("alisson")
+a = newPlayer("modoki")
+print(a)
